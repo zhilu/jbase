0181b57d4022f0d82bfef120cede8e3949983642
zhilu2000 <zhilu2000@163.com>
2018-06-24 21:30:03 +0800

    kohonen

---
+++ math/kohonen/src/main/java/shi/kohonen/DataPoint.java
@@ -0,0 +1,5 @@
+package shi.kohonen;
+
+public interface DataPoint {
+	public double [] getPoint();
+}
---
+++ math/kohonen/src/main/java/shi/kohonen/Net.java
@@ -0,0 +1,8 @@
+package shi.kohonen;
+
+public interface Net {
+
+	public void init();
+	public void train(DataSet ds);
+
+}
